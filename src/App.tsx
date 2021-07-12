3841b2340458ca637ca83ec7966e577a422227f2
Satoshi Matsushima <>
2021-07-12 17:49:44 +0900

    Lerning Optional Chaining

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import axios from 'axios';
 import { Todo } from './Todo';
 import { TodoType } from './types/toso';
 import { Text } from './Text';
+import { UserProfile } from './UserProfile';
+import { User } from './types/User';
+
+const user: User = {
+  name: 'じゃけぇ',
+  hobbies: ['映画', 'ゲーム'],
+};
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +24,7 @@ function App() {
 
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color={'red'} fontSize={'18px'} />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
