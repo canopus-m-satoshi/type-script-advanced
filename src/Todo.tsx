8efc2b62ae5abd00e25e3c59bc5620d1b18e07c7
Satoshi Matsushima <>
2021-07-12 17:06:41 +0900

    created types directory

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,9 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from './types/toso';
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  // props: Pick<TodoType, 'userId' | 'title' | 'completed'>, PICkで必要な情報のみ抽出
+  props: Omit<TodoType, 'id'>, // Omitで不要な情報を除く
+) => {
   const { title, userId, completed = false } = props;
   const comoletMark = completed ? '[完]' : '[未]';
 
